c80c31f01ea67b836b2aa8bc090a37ca739c843b
Ivan Yurin <i.yurin@czi-grif.ru>
2020-09-19 19:43:25 +0300

    master

--- common/assets/AssetBundle.php
+++ common/assets/AssetBundle.php
@@ -9,8 +9,15 @@ namespace common\assets;
 class AssetBundle extends \yii\web\AssetBundle
 {
     public $sourcePath = '@common/web';
-    public $css = [];
-    public $js = [];
+
+    public $css = [
+        'css/rusbeldoor-yii2-common.css',
+    ];
+
+    public $js = [
+        'js/rusbeldoor-yii2-common.js',
+    ];
+
     public $depends = [
         'rusbeldoor\yii2General\common\assets\AssetBundle',
     ];
