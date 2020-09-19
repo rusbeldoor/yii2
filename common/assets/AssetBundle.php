3fb2e8b9e7e764787febbdc107d8ae7c24aa3585
Ivan Yurin <i.yurin@czi-grif.ru>
2020-09-19 09:42:05 +0300

    master

--- common/assets/AssetBundle.php
+++ common/assets/AssetBundle.php
@@ -11,8 +11,5 @@ class AssetBundle extends \yii\web\AssetBundle
     public $sourcePath = '@common/web';
     public $css = [];
     public $js = [];
-    public $depends = [
-        'common\assets\fontawesome\AssetBundle',
-        'rusbeldoor\yii2General\common\assets\AssetBundle',
-    ];
+    public $depends = [];
 }
