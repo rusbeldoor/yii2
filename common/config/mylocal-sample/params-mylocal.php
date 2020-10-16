2849e67ec7392781810e1867f3c76993a6a95009
Ivan Yurin <i.yurin@czi-grif.ru>
2020-10-16 13:30:15 +0300

    master

--- common/config/mylocal-sample/params-mylocal.php
+++ common/config/mylocal-sample/params-mylocal.php
@@ -1,6 +1,15 @@
 <?php
 return [
     'masterPassword' => '12345678',
+
     'mailFrom' => 'noreply@yii2.local',
     'mailFromName' => 'Название проекта',
+
+    'rusbeldoor' => [ // Поставщик rusbeldoor
+        'yii2General' => [ // Репозиторий yii2General
+            'subscriptions' => [ // Модуль Subscriptions
+                'salt' => '12345678', // Соль для хэша
+            ],
+        ],
+    ],
 ];
