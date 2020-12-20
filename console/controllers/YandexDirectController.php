7561ff585856e1730ee7dad4d534249aeeffc660
Ivan Yurin <rusbeldoor@gmail.com>
2020-12-20 22:58:36 +0300

    master

---
+++ console/controllers/YandexDirectController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace console\controllers;
+
+/**
+ * Крон Яндекс.Директ
+ */
+class YandexDirectController extends \rusbeldoor\yii2General\console\controllers\YandexDirectController
+{
+    /**
+     * ...
+     */
+    public function actionIndex()
+    {
+        parent::actionIndex();
+
+        // Ваши дополнительные действия (если требуются)
+        // ...
+    }
+}
